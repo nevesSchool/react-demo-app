6581be8a1ff01236c5bd46604ce4bc07ddc740d5
Martim Neves <martim.neves@tectius.com>
2025-05-04 23:46:03 +0100

    Update: Title and Content

--- src/components/ExploreContainer.tsx
+++ src/components/ExploreContainer.tsx
@@ -5,8 +5,8 @@ interface ContainerProps { }
 const ExploreContainer: React.FC<ContainerProps> = () => {
   return (
     <div id="container">
-      <strong>Ready to create an app?</strong>
-      <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
+      <strong>My first React Ionic App!</strong>
+      <p>Check my projects on <a target="_blank" rel="noopener noreferrer" href="https://github.com/nevesSchool?tab=repositories">GitHub!</a></p>
     </div>
   );
 };
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,15 +7,10 @@ const Home: React.FC = () => {
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Blank</IonTitle>
+          <IonTitle>React Demo App</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
-          </IonToolbar>
-        </IonHeader>
         <ExploreContainer />
       </IonContent>
     </IonPage>
